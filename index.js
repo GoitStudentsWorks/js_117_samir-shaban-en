4fac9e16966ae79066fceb30e6e3c777124be2a6
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2025-07-13 18:59:56 +0000

    Deploying to gh-pages from @ GoitStudentsWorks/js_117_samir-shaban-en@02a54ec21cbc3f58087911900d6831c5a89f2526 🚀

---
+++ index.js
@@ -0,0 +1,119 @@
+import{a as W,A as F,S as Q,N as z,P as Y,i as q}from"./assets/vendor--MtLI3sW.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const n of document.querySelectorAll('link[rel="modulepreload"]'))s(n);new MutationObserver(n=>{for(const a of n)if(a.type==="childList")for(const l of a.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&s(l)}).observe(document,{childList:!0,subtree:!0});function o(n){const a={};return n.integrity&&(a.integrity=n.integrity),n.referrerPolicy&&(a.referrerPolicy=n.referrerPolicy),n.crossOrigin==="use-credentials"?a.credentials="include":n.crossOrigin==="anonymous"?a.credentials="omit":a.credentials="same-origin",a}function s(n){if(n.ep)return;n.ep=!0;const a=o(n);fetch(n.href,a)}})();const E=document.getElementById("burger-btn"),h=document.getElementById("navModal"),U=document.getElementById("closeMenu");E.addEventListener("click",()=>{h.classList.add("open"),E.classList.add("burger-hidden"),document.body.style.overflow="hidden"});U.addEventListener("click",()=>{h.classList.remove("open"),E.classList.remove("burger-hidden"),document.body.style.overflow="auto"});window.addEventListener("click",e=>{e.target===h&&(h.classList.remove("open"),document.body.style.overflow="auto")});const J=document.querySelectorAll(".nav-list a");J.forEach(e=>{e.addEventListener("click",t=>{e.getAttribute("data-link"),h.classList.remove("open"),E.classList.remove("burger-hidden"),document.body.style.overflow="auto"})});const C="https://books-backend.p.goit.global/books/",Z="https://books-backend.p.goit.global/books/category-list",G="https://books-backend.p.goit.global/books/top-books";async function w(e){try{return await W.get(e)}catch(t){console.error(t)}finally{}}async function X(e){const t=C+e;return await w(t)}async function ee(){return await w(Z)}async function te(){return await w(G)}async function oe(e){return await w(C+e)}async function ne(e){const t=C+"category?category="+e;return await w(t)}const M=document.querySelector(".modal-content");M.innerHTML="";function se(e){X(e).then(t=>{ae(t)}).catch(t=>console.log(t))}function ae(e){const t=e.data;try{const o={bookId:t._id,bookPicture:t.book_image,bookTitle:t.title,bookAuthor:t.author,bookPrice:t.price,bookQuantity:1,details:t.description,shipping:`We ship across the United States within 2–5 business days. All
+                orders are processed through USPS or a reliable courier service.
+                Enjoy free standard shipping on orders over $50.`,returns:`You can return an item within 14 days of receiving your order,
+                provided it hasn’t been used and is in its original condition.
+                To start a return, please contact our support team — we’ll guide
+                you through the process quickly and hassle-free.`};ie(o)}catch(o){console.error("Error loading:",o)}}function ie(e){const{bookId:t,bookPicture:o,bookTitle:s,bookAuthor:n,bookPrice:a,bookQuantity:l,details:f,shipping:i,returns:d}=e,p=`      
+        <button class="modal_close-btn">
+          <svg width="16" height="16">
+            <use href="#icon-x"></use>
+          </svg>
+        </button>
+          <picture class="modal-books_picture" id="bookPicture">
+          <img
+            class="modal-books_img"
+            src="${o}"
+            alt=""
+          />
+        </picture>
+        <div class="modal-books_data"
+          data-book-id="${t}"
+          data-book-price="${a}"
+          data-book-title="${s}"
+        >
+          <h2 class="modal-books_title">${s}</h2>
+          <p class="modal-books_author">${n}</p>
+          <p class="modal-books_price">$${a}</p>
+  
+          <form class="book-action-form" id="bookModalActionForm">
+            <div class="modal-books_quantity-container"
+              data-min-quantity="1" 
+              data-max-quantity="999" 
+              data-step="1"
+            >
+              <button class="btn btn-secondary"
+                type="button"
+                class="btn btn-secondary"
+                data-action="decrease"
+              >-</button>
+              <input
+                class="quantity-input"
+                id="bookQuantity"
+                value="${l}"
+                min="1"
+              />
+              <button class="btn btn-secondary"
+                type="button"
+                class="btn btn-secondary"
+                data-action="increase"
+              >+</button>
+            </div>
+            <div class="modal-books_buttons-container">
+              <button 
+                class="btn"
+                type="button"
+                data-action="add-to-cart"
+              >Add To Cart</button>
+              <button
+                type="submit"
+                class="btn btn-secondary"
+                data-action="buy-now"
+              >Buy Now</button>
+            </div>
+          </form>
+  
+          <div class="accordion-container">
+            <div class="ac">
+              <h2 class="ac-header">
+                <button type="button" class="ac-trigger">
+                  Details
+                  <svg class="chevron-icon">
+                    <use href="#icon-chevron-up"></use>
+                  </svg>
+                </button>
+              </h2>
+              <div class="ac-panel">
+                <p class="ac-text">${f}</p>
+              </div>
+            </div>
+  
+            <div class="ac">
+              <h2 class="ac-header">
+                <button type="button" class="ac-trigger">
+                  Shipping<svg class="chevron-icon">
+                    <use href="#icon-chevron-up"></use>
+                  </svg>
+                </button>
+              </h2>
+              <div class="ac-panel">
+                <p class="ac-text">${i}</p>
+              </div>
+            </div>
+  
+            <div class="ac">
+              <h2 class="ac-header">
+                <button type="button" class="ac-trigger">
+                  Returns<svg class="chevron-icon">
+                    <use href="#icon-chevron-up"></use>
+                  </svg>
+                </button>
+              </h2>
+              <div class="ac-panel">
+                <p class="ac-text">${d}</p>
+              </div>
+            </div>
+          </div>
+        </div>`;M.innerHTML=p,document.querySelector(".modal-books").classList.add("is-open"),document.body.style.overflow="hidden",document.querySelector(".modal").addEventListener("click",me),document.querySelector(".modal_close-btn").addEventListener("click",A),new F(document.querySelector(".modal-books .accordion-container"),{showMultiple:!0,duration:400,collapse:!0});const m=document.getElementById("bookModalActionForm");m.addEventListener("submit",N),m.addEventListener("click",R)}function N(e){e.preventDefault(),de()}function R(e){switch(e.target.dataset.action){case"decrease":re(e.target);break;case"increase":ce(e.target);break;case"add-to-cart":le(e.target);break}}function re(e){const t=e.closest("[data-min-quantity]"),o=t.querySelector(".quantity-input"),s=parseInt(t.dataset.minQuantity),n=parseInt(o.value);n>s&&(o.value=n-1)}function ce(e){const t=e.closest("[data-max-quantity]"),o=t.querySelector(".quantity-input"),s=parseInt(t.dataset.maxQuantity),n=parseInt(o.value);n<s&&(o.value=n+1)}function le(e){const t=e.closest("[data-book-id]"),o=t.dataset.bookId,s=parseInt(t.querySelector(".quantity-input").value);console.log(`Book with ID'${o}' in q-ty '${s}' added to Cart`)}function de(){console.log("Thanks for bying")}function ue(){const e=document.querySelectorAll(".modal-open");e.forEach(t=>t.addEventListener("click",T)),e.forEach(t=>t.addEventListener("keydown",pe))}let _="";function T(e){window.addEventListener("keydown",O),_=this.id,se(_)}function A(){window.removeEventListener("keydown",O),document.querySelector(".modal-books").classList.remove("is-open"),document.body.style.overflow="";const e=document.getElementById("bookModalActionForm");e.removeEventListener("submit",N),e.removeEventListener("click",R),M.innerHTML=""}function me(e){e.currentTarget===e.target&&A()}function O(e){e.code==="Escape"&&A()}function pe(e){e.code==="Enter"&&T()}const r={categoriesListenRef:document.querySelector(".js-listener-categories"),categoriesListRef:document.querySelector(".js-list-categories"),btnAddShopList:document.querySelector(".js-button-addlist"),btnloadMore:document.querySelector(".load-more"),loader:document.querySelector(".loader"),currentBooksShowed:document.querySelector(".current-number-books"),totalBooks:document.querySelector(".total-number-books"),booksBoxRef:document.querySelector(".js-list-books")};r.categoriesListenRef.addEventListener("click",B);let g=[],k=1,y=window.innerWidth,u=10,L=0;addEventListener("resize",()=>{(window.innerWidth>767&&y<768||window.innerWidth>1439&&y<1440||window.innerWidth<1440&&y>1439||window.innerWidth<768&&y>767)&&location.reload()});y<768?u=10:(y>767&&y<1440,u=24);const fe=u;te().then(e=>{I(e)}).catch(e=>console.log(e));ee().then(e=>{be(e)}).catch(e=>console.log(e));function ye(){r.loader.style.display="block"}function P(){r.loader.style.display="none"}function B(e){r.booksBoxRef.innerHTML="",ye(),V();let t=e.target.getAttribute("data-name");u=fe,t==="top-books"?oe(t).then(o=>{I(o)}).catch(o=>console.log(o)).finally(()=>P()):ne(t).then(o=>{ke(o)}).catch(o=>console.log(o)).finally(()=>P())}function be(e){const o=e.data.map(s=>`<li class="item" data-name="${s.list_name}" tabindex="0">${s.list_name}
+		</li>`).join("");r.categoriesListRef.insertAdjacentHTML("beforeend",o),window.innerWidth<1440&&ge()}function ge(){const e=r.categoriesListRef,t=e.querySelectorAll("li"),o=document.createElement("div");o.className="custom-dropdown";const s=document.createElement("div");s.className="selected",s.textContent="Select Category";const n=document.createElement("ul");n.className="dropdown-list",t.forEach(a=>{const l=document.createElement("li");l.textContent=a.textContent.trim(),l.setAttribute("data-name",a.getAttribute("data-name")),n.appendChild(l)}),o.appendChild(s),o.appendChild(n),e.parentNode.appendChild(o,e),e.style.display="none",s.addEventListener("click",()=>{n.style.display=n.style.display==="block"?"none":"block"}),document.addEventListener("click",a=>{o.contains(a.target)||(n.style.display="none")}),n.addEventListener("click",a=>{a.target.tagName==="LI"&&(s.textContent=a.target.textContent.trim(),a.target.getAttribute("data-name"),B(a),n.style.display="none")}),r.categoriesListenRef.removeEventListener("click",B)}function I(e){r.booksBoxRef.innerHTML="",g=[];for(const t of e.data){const o=t.books;for(let s=0;s<o.length;s+=1)g.push(o[s])}k=1,L=0,x()}function ke(e){if(r.booksBoxRef.innerHTML="",e.request.responseURL.includes("top-books")){I(e);return}g=e.data,k=1,L=0,x()}function x(){let e="";const t=g.slice((k-1)*u,(k-1)*u+u);for(const o of t){let s=ve(o);e+=s,L++}r.booksBoxRef.insertAdjacentHTML("beforeend",e),k*u<g.length?he():V(),r.totalBooks.textContent=g.length,r.currentBooksShowed.textContent=L,ue()}function ve(e){return` <li class="item book-info " tabindex="0" >
+
+			 <img class='book-img' src="${e.book_image}" alt="${e.title}" class="img">
+
+  <div class="book-content">
+	<p class="subject">${e.title.toLowerCase()}</p>
+  <p class="price">$${e.price}</p>
+  </div>
+	<p class="author">${e.author}</p>
+    
+    <button class="button modal-open" id="${e._id}">Learn More</button>
+</li>`}function he(){r.btnloadMore.style.display="block"}function V(){r.btnloadMore.style.display="none"}r.btnloadMore.addEventListener("click",e=>{k+=1,u=4,x()});document.querySelector(".scroll-btn");const c=new Q(".swiper",{modules:[z,Y],spaceBetween:25,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".my-swiper ",type:"bullets"}});window.addEventListener("DOMContentLoaded",j);document.querySelector(".right");document.querySelector(".left");window.addEventListener("resize",j);function j(){const e=window.innerWidth;e>=1440?(c[1].params.slidesPerView=3,c[1].update(),c[2].params.slidesPerView=3,c[2].update()):e>=768?(c[1].params.slidesPerView=2,c[1].update(),c[2].params.slidesPerView=2,c[2].update()):e<768&&(c[1].params.slidesPerView=1,c[1].update(),c[2].params.slidesPerView=1,c[2].update())}const K=document.querySelector(".footer-form"),S=document.querySelector(".footer-input"),D=document.querySelector("#errorMessage");let v={email:""};K.addEventListener("input",we);function we(e){e.target.nodeName==="INPUT"&&(v.email=e.target.value),localStorage.setItem("email-state",JSON.stringify(v))}if(localStorage.getItem("email-state")!==null){let e=JSON.parse(localStorage.getItem("email-state"));v.email=e.email,document.querySelector('[name="footer-input"]').value=e.email}else v.email="";K.addEventListener("submit",Ee);function Ee(e){e.preventDefault(),v.email===""||!S.checkValidity()?(S.classList.add("invalid"),D.style.display="block"):(v.email="",document.querySelector('[name="footer-input"]').value="",localStorage.removeItem("email-state"),S.classList.remove("invalid"),D.style.display="none",q.show({message:"Email submitted successfully!",position:"topRight",backgroundColor:"#59A10D",titleColor:"#fff",messageColor:"#fff",close:!0}))}document.addEventListener("DOMContentLoaded",function(){const e=document.getElementById("contact-modal"),t=document.getElementById("closeModalBtn"),o=document.querySelectorAll(".open-modal"),s=document.querySelector(".contact-modal-form");if(!s||!e||!t)return;o.forEach(i=>{i.addEventListener("click",d=>{d.preventDefault(),e.style.display="flex",document.body.style.overflow="hidden"})}),t.addEventListener("click",n),document.addEventListener("keydown",i=>{i.key==="Escape"&&n()}),e.addEventListener("click",i=>{i.target===e&&n()});function n(){e.style.display="none",document.body.style.overflow=""}s.addEventListener("submit",function(i){i.preventDefault(),s.querySelectorAll("input, textarea").forEach(H=>H.classList.remove("input-error"));const p=s.querySelector("#name"),m=s.querySelector("#email"),$=s.querySelector("#message");let b=!0;p.value.trim()?l(p.value)||(f(p,"Please enter a valid name"),b=!1):(f(p,"Name is required"),b=!1),m.value.trim()?a(m.value)||(f(m,"Enter a valid email"),b=!1):(f(m,"Email is required"),b=!1),$.value.length>500&&(f($,"Message is too long (max 500 characters)"),b=!1),b&&(q.success({title:"Success",message:"Form submitted successfully!",position:"topRight",timeout:3e3}),s.reset(),n())});function a(i){return/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(i)}function l(i){const d=i.trim(),p=/^[A-Za-zА-Яа-яЁё\s\-]+$/,m=/[aeiouаеёиоуыэюя]/i;return d.length>=2&&p.test(d)&&m.test(d)}function f(i,d){i.classList.add("input-error"),i.placeholder=d,q.error({title:"Error",message:d,position:"topRight",timeout:3e3})}});
+//# sourceMappingURL=index.js.map
